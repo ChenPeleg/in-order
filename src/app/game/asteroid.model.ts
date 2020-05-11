29056b5e0176d92afe3977df1c7f7bd7b6bd0618
חן פלג <chenpeleg@gmail.com>
2020-05-11 11:56:46 +0300

    wrong answer results in red astroid and correct in exploding

--- src/app/game/asteroid.model.ts
+++ src/app/game/asteroid.model.ts
@@ -4,12 +4,14 @@ export class Asteroid {
     public text: string;
     public index: number;
     public destroy: boolean;
+    public hot?: boolean;
     constructor(left: number, bottom: number, text: string, index: number, destroy: boolean) {
         this.left = left;
         this.bottom = bottom;
         this.text = text;
         this.index = index;
         this.destroy = false;
+        this.hot = false;
 
     }
 } 
--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -12,24 +12,37 @@ export class GameComponent implements OnInit {
   asteroids: Asteroid[];
   questionNumber: number
   currenQuestionText: string;
+  nextCorrect: number;
 
   constructor() {
     this.questionNumber = 1;
+    this.nextCorrect = 0;
     this.asteroids = [
       { left: 10, bottom: 20, text: data.questions[1].answers[0], index: 0, destroy: false },
-      { left: 20 /*40 */, bottom: 50, text: data.questions[1].answers[1], index: 1, destroy: false },
+      { left: 40, bottom: 50, text: data.questions[1].answers[1], index: 1, destroy: false },
       { left: 70, bottom: 20, text: data.questions[1].answers[2], index: 2, destroy: false }];
   }
   ngOnInit(): void {
     console.log(data.questions[1].text)
     this.currenQuestionText = data.questions[1].text
   }
-  asteroidClickHandler(clickData: { index: number }) {
+  asteroidClickHandler(clickData: { index: number }): void {
+    if (clickData.index === this.nextCorrect) { this.correctHandler(clickData.index) } else { this.wrongHandler(clickData.index) }
 
-    // this.asteroids.splice(clickData.index, 1);
-    this.asteroids[clickData.index].destroy = true
-    console.log(clickData.index, this.asteroids)
-
+  }
+  correctHandler(num: number): void {
+    this.asteroids[num].destroy = true
+    this.nextCorrect = this.nextCorrect + 1;
+    while (this.nextCorrect < this.asteroids.length && this.asteroids[this.nextCorrect].destroy) {
+      this.nextCorrect++
+    }
+    console.log(this.nextCorrect)
+    if (this.nextCorrect === this.asteroids.length) {
+      alert('next Question')
+    }
+  }
+  wrongHandler(num: number): void {
+    this.asteroids[num].hot = true
   }
 
 }
