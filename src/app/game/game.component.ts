96eea1e1c8c4a210303767d3ca59d4ff7646d25e
חן פלג <chenpeleg@gmail.com>
2020-05-11 21:17:43 +0300

    strating to experiment with orderning asteroid function

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -17,18 +17,39 @@ export class GameComponent implements OnInit {
   constructor() {
     this.questionNumber = 1;
     this.nextCorrect = 0;
-    this.asteroids = [
-      { left: 10, bottom: 20, text: data.questions[1].answers[0], index: 0, destroy: false },
-      { left: 40, bottom: 50, text: data.questions[1].answers[1], index: 1, destroy: false },
-      { left: 70, bottom: 20, text: data.questions[1].answers[2], index: 2, destroy: false }];
   }
   ngOnInit(): void {
     console.log(data.questions[1].text)
-    this.currenQuestionText = data.questions[1].text
+    this.currenQuestionText = data.questions[1].text;
+    this.asteroids = this.setAstroidData()
+
+
   }
   asteroidClickHandler(clickData: { index: number }): void {
     if (clickData.index === this.nextCorrect) { this.correctHandler(clickData.index) } else { this.wrongHandler(clickData.index) }
 
+  }
+  setAstroidData(): Array<Asteroid> {
+    const namesArr: Array<string> = ['one', 'two', "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+
+    const steps: number = 10;
+    const centerX = 50;
+    const centerY = 50;
+    const radius = 35;
+    let xValues: Array<number> = []
+    let yValues: Array<number> = []
+    for (var i: number = 0; i < steps; i++) {
+      xValues[i] = (centerX + radius * Math.cos(2 * Math.PI * i / steps));
+      yValues[i] = (centerY + radius * Math.sin(2 * Math.PI * i / steps));
+    }
+    const arr1: Array<Asteroid> = namesArr.map(n => { return { left: namesArr.indexOf(n) * 8, bottom: 20, text: n, index: namesArr.indexOf(n), destroy: false } })
+
+    let arr = [...arr1];
+    //  [
+    //   { left: 10, bottom: 20, text: data.questions[1].answers[0], index: 0, destroy: false },
+    //   { left: 40, bottom: 50, text: data.questions[1].answers[1], index: 1, destroy: false },
+    //   { left: 70, bottom: 20, text: data.questions[1].answers[2], index: 2, destroy: false }];
+    return arr
   }
   correctHandler(num: number): void {
     this.asteroids[num].destroy = true
