bc9199a16986c17794f1edf713a9daeb9301167b
חן פלג <chenpeleg@gmail.com>
2020-06-05 14:20:42 +0300

    adding summary model 2

--- src/app/models/summary.model.ts
+++ src/app/models/summary.model.ts
@@ -1,14 +1,8 @@
-export class SummaryModel {
-    public q0: number;
-    public q1: number;
-    public q23: number;
-
-
-    constructor(q0: number,
-        q1: number,
-        q23: number) {
-        this.q0 = q0
-        this.q1 = q1
-        this.q23 = q23;
-    }
-} 
+export interface SummaryModel {
+    q0: number
+    q1: number
+    q23: number
+    p0: number
+    p1: number
+    p23: number
+}
--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -1,12 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-// interface SummUpIntreface {
-//   q0: number
-//   q1: number
-//   q23: number
-//   p0: number
-//   p1: number
-//   p23: number
-// }
+import { SummaryModel } from "../models/summary.model"
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
@@ -16,13 +9,11 @@ export class SummaryComponent implements OnInit {
 
   // num: object = { q0: 0, q1: 1, q23: 0 };
   // pre: object = { q0: 0, q1: 1, q23: 2 };
-  // summup: SummUpIntreface = { q0: 0, q1: 1, q23: 0, p0: 0, p1: 1, p23: 0 }
+  summup: SummaryModel = { q0: 0, q1: 1, q23: 0, p0: 0, p1: 1, p23: 0 }
   num: number;
   pre: number;
-  constructor(
-    // summup: SummUpIntreface
-  ) {
-    // this.summup = summup;
+  constructor() {
+    this.summup = { q0: 0, q1: 1, q23: 0, p0: 0, p1: 1, p23: 0 }
     this.num = 4;
     this.pre = 2;
 
