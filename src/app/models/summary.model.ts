0a6b19277fb4c25527dc3924c7a105922c31c266
chenp <chenpeleg@gmail.com>
2023-06-09 14:32:54 +0300

    deleted old project

--- src/app/models/summary.model.ts
+++
@@ -1,9 +0,0 @@
-export interface SummaryModel {
-  q0: number
-  q1: number
-  q23: number
-  p0: number
-  p1: number
-  p23: number
-  text?: string[];
-}
--- src/app/summary/spread.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import {SpreadPipe} from './spread.pipe';
-
-describe('SpreadPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SpreadPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
